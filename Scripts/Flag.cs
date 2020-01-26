ae253747d5d1a098acc67a10eb9f5dd71636b23e
Geekuccino <60325051+Geekuccino@users.noreply.github.com>
2020-01-26 10:52:16 -0800

    Add main files

---
+++ Scripts/Flag.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Flag : MonoBehaviour
+{
+    public bool captured;
+    public float timeToCapture;
+    public float timeSoFar;
+
+    public Image UI;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeSoFar > timeToCapture)
+        {
+            captured = true;
+            UI.gameObject.SetActive (true);
+        }
+    }
+    void OnTriggerStay(Collider collider)
+    {
+            if (collider.gameObject.tag == "Player") {
+                timeSoFar += Time.deltaTime;
+            }
+    }
+    void OnTriggerExit(Collider collider)
+    {
+            if (collider.gameObject.tag == "Player")
+            {
+                if (captured == false)
+                {
+                    timeSoFar = 0;
+                }
+            }
+      
+    }
+}
---
+++ Scripts/FlagManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FlagManager : MonoBehaviour
+{
+    Flag[] flagsInLevel;
+    float flagsToWin;
+    public string nextLevel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        flagsInLevel = FindObjectsOfType<Flag>();
+        flagsToWin = flagsInLevel.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // check flags are captured
+        if (CheckFlags())
+        {
+            SceneManager.LoadScene(nextLevel);
+        }
+    }
+
+    bool CheckFlags()
+    {
+        bool result = true;
+        foreach (Flag f in flagsInLevel)
+        {
+            if (f.captured == false)
+            {
+                result = false;
+            }
+        }
+        return result;
+    }
+}
